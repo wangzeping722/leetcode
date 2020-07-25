bbc20a0875b2785dc0be1def1ec2dbccd529e09d
wangzeping <werorong@gmail.com>
2020-07-25 22:35:43 +0800

    复习排序算法

--- sorts/bubble.go
+++ sorts/bubble.go
@@ -5,84 +5,6 @@ package sorts
 	冒泡排序只会操作相邻的两个数据，每次冒泡操作都会对相邻的两个元素进行比较，看是否满足大小关系，
 	如果不满足就互换位置。
 */
-//
-//func Bubble(arr []int) {
-//	if len(arr) <= 1 {
-//		return
-//	}
-//	for i := len(arr) - 1; i > 0; i-- {
-//		f := false
-//		for j := 1; j <= i; j++ {
-//			if arr[j] < arr[j-1] {
-//				f = true
-//				arr[j], arr[j-1] = arr[j-1], arr[j]
-//			}
-//		}
-//		if !f {
-//			break
-//		}
-//	}
-//}
-
-//func Bubble(arr []int) {
-//	if len(arr) <= 1 {
-//		return
-//	}
-//	for i := len(arr) - 1; i > 0; i-- {
-//		flag := false // 哨兵
-//		for j := 1; j <= i; j++ {
-//			if arr[j] < arr[j-1] {
-//				flag = true
-//				arr[j], arr[j-1] = arr[j-1], arr[j]
-//			}
-//		}
-//
-//		if !flag {
-//			return
-//		}
-//	}
-//}
-//
-//func Bubble1(arr []int) {
-//	if len(arr) <= 1 {
-//		return
-//	}
-//
-//	for i := len(arr) - 1; i > 0; i-- {
-//		flag := true
-//		for j := 1; j <= i; j++ {
-//			if arr[j] < arr[j-1] {
-//				flag = false
-//				arr[j], arr[j-1] = arr[j-1], arr[j]
-//			}
-//		}
-//		if flag {
-//			return
-//		}
-//	}
-//}
-//
-//
-//func Bubble2(arr []int) {
-//	if len(arr) <= 1 {
-//		return
-//	}
-//
-//
-//	for i := len(arr) - 1; i > 0; i-- {
-//		flag := true
-//		for j := 1; j <= i; j++ {
-//			if arr[j] < arr[j-1] {
-//				flag = false
-//				arr[j], arr[j-1] = arr[j-1], arr[j]
-//			}
-//		}
-//		if flag {
-//			return
-//		}
-//	}
-//}
-
 func Bubble(arr []int) {
 	if len(arr) <= 1 {
 		return
@@ -102,12 +24,12 @@ func Bubble(arr []int) {
 	}
 }
 
-func bubble(arr []int) {
+func bubble1(arr []int) {
 	if len(arr) <= 1 {
 		return
 	}
 
-	for i := len(arr) - 1; i > 0; i-- {
+	for i := len(arr)-1; i > 0; i-- {
 		flag := true
 		for j := 1; j <= i; j++ {
 			if arr[j] < arr[j-1] {
--- sorts/merge.go
+++ sorts/merge.go
@@ -5,88 +5,6 @@ package sorts
 	如果要排序一个数组，我们先把数组从中间分成前后两个部分，然后对前后两部分分别排序，
 	再将排好序的两部分合并在一起
 */
-
-//func MergeSort(arr []int) {
-//	if len(arr) <= 1 {
-//		return
-//	}
-//	mergeSort(arr, 0, len(arr)-1)
-//}
-//
-//func mergeSort(arr []int, low, high int) {
-//	if low >= high {
-//		return
-//	}
-//
-//	mid := low + (high-low)>>1
-//	mergeSort(arr, low, mid)
-//	mergeSort(arr, mid+1, high)
-//	merge(arr, low, mid, high)
-//}
-//
-//func merge(arr []int, low, mid, high int) {
-//	length := high - low + 1
-//	tempArr := make([]int, length)
-//
-//	i, ileft, iright := 0, low, mid+1
-//	for ileft <= mid && iright <= high {
-//		if arr[ileft] <= arr[iright] {
-//			tempArr[i] = arr[ileft]
-//			ileft++
-//		} else {
-//			tempArr[i] = arr[iright]
-//			iright++
-//		}
-//		i++
-//	}
-//
-//	// copy the rest data
-//	if ileft <= mid {
-//		copy(tempArr[i:], arr[ileft:mid+1])
-//	}
-//	if iright <= high {
-//		copy(tempArr[i:], arr[iright:high+1])
-//	}
-//	copy(arr[low:high+1], tempArr[:])
-//}
-//
-//func MergeSort(arr []int) {
-//	if len(arr) <= 1 {
-//		return
-//	}
-//
-//	mid := len(arr) / 2
-//	MergeSort(arr[:mid])
-//	MergeSort(arr[mid:])
-//	merge(arr)
-//}
-//
-//func merge(arr []int) {
-//	length := len(arr)
-//	tempArr := make([]int, length)
-//	left, mid := 0, length/2-1
-//	right := mid+1
-//	i := 0
-//	for left <= mid && right <= length-1 {
-//		if arr[left] <= arr[right] {
-//			tempArr[i] = arr[left]
-//			left++
-//		} else {
-//			tempArr[i] = arr[right]
-//			right++
-//		}
-//		i++
-//	}
-//
-//	if left <= mid {
-//		copy(tempArr[i:], arr[left:mid+1])
-//	}
-//	if right <= length-1 {
-//		copy(tempArr[i:], arr[right:length])
-//	}
-//	copy(arr, tempArr)
-//}
-
 func MergeSort(arr []int) {
 	if len(arr) < 2 {
 		return
@@ -121,15 +39,4 @@ func merge(arr []int) {
 		copy(tempArr[i:], arr[right:])
 	}
 	copy(arr, tempArr)
-}
-
-func mergeSort(arr []int) {
-	if len(arr) <= 1 {
-		return
-	}
-
-	mid := len(arr) / 2
-	mergeSort(arr[:mid])
-	mergeSort(arr[mid:])
-	merge(arr)
 }
